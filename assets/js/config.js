54d680b4f13128a03aa15ae02e3b84c611a85c0f
mikaelnakano-creator <mikaelnakano@gmail.com>
2026-06-25 15:13:58 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-const CHECKOUT_URL = "https://SEU-LINK-DA-KIWIFY-AQUI";
+const CHECKOUT_URL = "https://pay.kiwify.com.br/8r0qZs6";
 
 function goToCheckout() {
   window.location.href = CHECKOUT_URL;
